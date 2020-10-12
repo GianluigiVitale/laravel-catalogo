6c41470f1802a015c0a28e4e965fc071064d95aa
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-10-12 19:50:30 +0200

    migration

---
+++ database/migrations/2020_10_12_174343_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->float('price', 8, 2);
+            $table->string('img');
+            $table->string('slug')->unique();
+            $table->boolean('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
