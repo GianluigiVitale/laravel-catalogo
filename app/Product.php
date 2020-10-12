c5c3089bb175cef1e59d5d18dc88fc5818ee5f5e
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-10-12 19:51:56 +0200

    model

---
+++ app/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'img',
+        'slug',
+        'published',
+    ];
+
+}
