7db69fe8a05b7dc7f666e5bf0cbf03b05647a218
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-10-13 10:03:09 +0200

    created crud views, draft of index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Product;
 
 use Illuminate\Http\Request;
 
@@ -13,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return view('products.index', compact('products'));
     }
 
     /**
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <th>Nome</th>
+                        <th>Descrizione</th>
+                        <th colspan="3">Azioni</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $product)
+                        <tr>
+                            <td>{{$product->name}}</td>
+                            <td>{{$product->description}}</td>
+                            <td>{{$product->price}}</td>
+                            <img src="{{$product->img}}">
+                            <td><a href="{{route('products.edit', $product->id)}}">Modifica</a></td>
+                            <td><a href="{{route('products.show', $product->slug)}}">Visualizza</a></td>
+                            <td>
+                                <form action="{{route('products.destroy', $product->id)}}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit">Elimina</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
